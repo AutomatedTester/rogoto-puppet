234ecec861ca578bc9b75359f5825e1d0a4dcc76
AutomatedTester <dburns@mozilla.com>
2014-04-08 00:11:46 +0100

    clean up of manifest, no functional changes

--- manifests/default.pp
+++ manifests/default.pp
@@ -5,23 +5,26 @@ package { "apache2":
   ensure  => present,
   require => Exec["apt-get update"],
 }
-package { "python-dev":
-  ensure => installed,
-  require => Exec["apt-get update"]
-}
-package { "python-pip":
-  ensure => installed,
-  require => Exec["apt-get update"]
-}
-package { 'git':
-  ensure => installed,
-  require => Exec["apt-get update"]
-}
+
 service { "apache2":
   ensure  => "running",
   require => Package["apache2"],
 }
 
+package { "python-dev":
+  ensure => installed,
+  require => Exec["apt-get update"]
+}
+
+package { "python-pip":
+  ensure => installed,
+  require => Exec["apt-get update"]
+}
+
+package { 'git':
+  ensure => installed,
+  require => Exec["apt-get update"]
+}
 
 vcsrepo { "/tmp/rogoto-http/":
     require => Package['git'],
